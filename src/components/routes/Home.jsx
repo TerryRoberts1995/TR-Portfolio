2d152c02bfbadf28dd83a01cd8be6b7f095fc6cd
Terry Roberts <terryjbroberts@yahoo.com>
2023-05-20 03:42:10 -0500

    Added basic SCSS files, and added basic structure for Home/landing page.

--- src/components/routes/Home.jsx
+++ src/components/routes/Home.jsx
@@ -4,7 +4,35 @@ import "../../styles/home.scss";
 const Home = () => {
   return (
     <>
-      <h1>This the Landing page.</h1>
+      <div className="header-container">
+        <h1 className="header">Hello World!</h1>
+      </div>
+
+      <div className="content-section">
+        <div className="content-box-one">
+          <div className="image-box">
+            <img src="https://example.com/example-image.jpg" alt="Example" />
+          </div>
+          <div className="text-box">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut
+              magna vitae lacus scelerisque efficitur eget ac nisl.
+            </p>
+          </div>
+        </div>
+
+        <div className="content-box-two">
+          <div className="image-box">
+            <img src="https://example.com/example-image.jpg" alt="Example" />
+          </div>
+          <div className="text-box">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean ut
+              magna vitae lacus scelerisque efficitur eget ac nisl.
+            </p>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
